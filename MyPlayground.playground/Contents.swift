307dfbb400232f737635536d0721e10e24f386d7
Spiridonov Maxim <taijased@gmail.com>
2019-12-02 11:52:30 +0300

    init

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,93 @@
+import UIKit
+
+
+// color hex init
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        assert(red >= 0 && red <= 255, "Invalid red component")
+        assert(green >= 0 && green <= 255, "Invalid green component")
+        assert(blue >= 0 && blue <= 255, "Invalid blue component")
+        self.init(red: CGFloat(red) / 255.0, green: CGFloat(green) / 255.0, blue: CGFloat(blue) / 255.0, alpha: 1.0)
+    }
+    
+    convenience init(netHex:Int) {
+        self.init(red:(netHex >> 16) & 0xff, green:(netHex >> 8) & 0xff, blue:netHex & 0xff)
+    }
+}
+
+
+
+// Colors structure
+
+extension UIColor {
+    enum ExampleColor {
+        enum Green {
+            static let Fern = UIColor(netHex: 0x6ABB72)
+            static let MountainMeadow = UIColor(netHex: 0x3ABB9D)
+            static let ChateauGreen = UIColor(netHex: 0x4DA664)
+            static let PersianGreen = UIColor(netHex: 0x2CA786)
+        }
+        
+        enum Blue {
+            static let PictonBlue = UIColor(netHex: 0x5CADCF)
+            static let Mariner = UIColor(netHex: 0x3585C5)
+            static let CuriousBlue = UIColor(netHex: 0x4590B6)
+            static let Denim = UIColor(netHex: 0x2F6CAD)
+            static let Chambray = UIColor(netHex: 0x485675)
+            static let BlueWhale = UIColor(netHex: 0x29334D)
+        }
+        
+        enum Violet {
+            static let Wisteria = UIColor(netHex: 0x9069B5)
+            static let BlueGem = UIColor(netHex: 0x533D7F)
+        }
+        
+        enum Yellow {
+            static let Energy = UIColor(netHex: 0xF2D46F)
+            static let Turbo = UIColor(netHex: 0xF7C23E)
+        }
+        
+        enum Orange {
+            static let NeonCarrot = UIColor(netHex: 0xF79E3D)
+            static let Sun = UIColor(netHex: 0xEE7841)
+        }
+        
+        enum Red {
+            static let TerraCotta = UIColor(netHex: 0xE66B5B)
+            static let Valencia = UIColor(netHex: 0xCC4846)
+            static let Cinnabar = UIColor(netHex: 0xDC5047)
+            static let WellRead = UIColor(netHex: 0xB33234)
+        }
+        
+        enum Gray {
+            static let AlmondFrost = UIColor(netHex: 0xA28F85)
+            static let WhiteSmoke = UIColor(netHex: 0xEFEFEF)
+            static let Iron = UIColor(netHex: 0xD1D5D8)
+            static let IronGray = UIColor(netHex: 0x75706B)
+        }
+    }
+}
+
+
+// Colors structrure
+
+extension UIColor {
+    enum Separator {
+        static var tableElements: UIColor {
+            return UIColor.ExampleColor.Blue.BlueWhale
+        }
+        static var bars: UIColor {
+            return UIColor.ExampleColor.Gray.Iron
+        }
+    }
+    
+    enum Cards {
+        static var search: UIColor {
+            return UIColor.ExampleColor.Red.TerraCotta
+        }
+        static var main: UIColor {
+            return UIColor.ExampleColor.Violet.Wisteria
+        }
+    }
+}
